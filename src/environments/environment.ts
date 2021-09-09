ea77ff04bfee21228155f39ea9a2d333b134c722
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-09 10:02:01 -0300

    configuração do firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDk1ZzFsbYM4fUA-borNuKVDeH6ElrEZvo",
+    authDomain: "fcamara-squad3-6ad63.firebaseapp.com",
+    projectId: "fcamara-squad3-6ad63",
+    storageBucket: "fcamara-squad3-6ad63.appspot.com",
+    messagingSenderId: "989608993670",
+    appId: "1:989608993670:web:d14cbcf4ebe65e0c1dc207"
+  }
 };
 
 /*
