efa1154e3685c55c107d4332c1cb59facd6ae27a
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-09 12:15:46 -0300

    implementação dos dados do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDk1ZzFsbYM4fUA-borNuKVDeH6ElrEZvo",
+    authDomain: "fcamara-squad3-6ad63.firebaseapp.com",
+    projectId: "fcamara-squad3-6ad63",
+    storageBucket: "fcamara-squad3-6ad63.appspot.com",
+    messagingSenderId: "989608993670",
+    appId: "1:989608993670:web:d14cbcf4ebe65e0c1dc207"
+  }
 };
