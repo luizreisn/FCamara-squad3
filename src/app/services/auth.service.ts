0299bafb0983283ae9c128b7f5ecd1c8a3c59a97
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-09 12:44:14 -0300

    configuração da service de auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Usuario } from '../interfaces/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public usuarioColecao: AngularFirestoreCollection<Usuario>;
+
+  constructor(private afa: AngularFireAuth, private afs: AngularFirestore) { 
+    this.usuarioColecao = this.afs.collection<Usuario>('Usuarios');
+  }
+
+  public getAuth(){
+    return this.afa;
+  }
+
+  public getUsuario(id:string){
+    return this;this.usuarioColecao.doc<Usuario>(id).valueChanges();
+  }
+
+  public cadastrar(usuarioCadastro: Usuario){
+    return this.afa.createUserWithEmailAndPassword(usuarioCadastro.email, usuarioCadastro.senha);
+  }
+
+  public login(usuarioLogin: Usuario){
+    return this.afa.signInWithEmailAndPassword(usuarioLogin.email, usuarioLogin.senha);
+  }
+
+  public sair(){
+    return this.afa.signOut();
+  }
 }
