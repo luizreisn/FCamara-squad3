1739d2c150b73a6a00c7f849a47cdb50d8639d1d
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-10 19:12:43 -0300

    criação de um botão 'sair' para testes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private authService: AuthService,
+    private alertCtrl: AlertController) {}
+
+  public async sair(){
+    const alertSair = await this.alertCtrl.create({
+      header: 'Deseja mesmo sair?',
+      buttons: [{
+        text: 'Cancelar'
+      },{
+        text: 'Sair',
+        handler: () => {
+          this.authService.sair();
+        }
+      }]
+    })
+    await alertSair.present();
+  }
 
 }
