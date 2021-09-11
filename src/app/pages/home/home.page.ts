c0406f1c6a7c9ac58067952b804559b6e2ef0bca
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-11 17:28:19 -0300

    Atualização e introduzir os dados do usuario na pagina home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Usuario } from 'src/app/interfaces/usuario';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -9,15 +11,32 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class HomePage {
 
-  constructor(private authService: AuthService,
-    private alertCtrl: AlertController) {}
+  public usuario: Usuario = null;
+  public usuarioId: string = null;
+  private usuarioSubscription: Subscription;
 
-  public async sair(){
+  constructor(private authService: AuthService,
+    private alertCtrl: AlertController) {
+    this.carregarDados();
+  }
+
+  public async carregarDados() {
+    this.usuarioId = (await this.authService.getAuth().currentUser).uid;
+    this.usuarioSubscription = this.authService.getUsuario(this.usuarioId).subscribe(data => {
+      this.usuario = data;
+    })
+  }
+
+  ngOnDestroy() {
+    this.usuarioSubscription.unsubscribe();
+  }
+
+  public async sair() {
     const alertSair = await this.alertCtrl.create({
       header: 'Deseja mesmo sair?',
       buttons: [{
         text: 'Cancelar'
-      },{
+      }, {
         text: 'Sair',
         handler: () => {
           this.authService.sair();
