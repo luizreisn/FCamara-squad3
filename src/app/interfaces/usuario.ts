c9b15f9438f09bf4ae8db685ea28b4aace3d7e88
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-12 23:15:58 -0300

    atualização da interface de usuario

--- src/app/interfaces/usuario.ts
+++ src/app/interfaces/usuario.ts
@@ -1,5 +1,12 @@
+import { Agendamento } from "./agendamento";
+
 export interface Usuario {
+    id?: string;
     nome?: string;
+    sobrenome?: string;
+    celular?: number;
+    cpf?: number;
     email?: string;
     senha?: string;
+    agendamentos?: Agendamento[];
 }
