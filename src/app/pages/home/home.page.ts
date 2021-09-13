aef602dbf6fcea5f8ddad3c61da2175be5146751
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-12 23:26:03 -0300

    implementação do menu lateral e atualização na home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
+import { BotaoMenu } from 'src/app/interfaces/botao-menu';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { AuthService } from 'src/app/services/auth.service';
+import { BotaoMenuService } from 'src/app/services/botao-menu.service';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +17,12 @@ export class HomePage {
   public usuarioId: string = null;
   private usuarioSubscription: Subscription;
 
+  public botoesMenu = new Array<BotaoMenu>();
+  private botoesMenuSubscription: Subscription;
+
   constructor(private authService: AuthService,
-    private alertCtrl: AlertController) {
+    private alertCtrl: AlertController,
+    private botaoMenuService: BotaoMenuService) {
     this.carregarDados();
   }
 
@@ -24,11 +30,15 @@ export class HomePage {
     this.usuarioId = (await this.authService.getAuth().currentUser).uid;
     this.usuarioSubscription = this.authService.getUsuario(this.usuarioId).subscribe(data => {
       this.usuario = data;
+    });
+    this.botoesMenuSubscription = this.botaoMenuService.getBotoesMenu().subscribe(data => {
+      this.botoesMenu = data;
     })
   }
 
   ngOnDestroy() {
     this.usuarioSubscription.unsubscribe();
+    this.botoesMenuSubscription.unsubscribe();
   }
 
   public async sair() {
