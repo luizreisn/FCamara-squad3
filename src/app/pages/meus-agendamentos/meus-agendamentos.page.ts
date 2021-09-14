41e807e02d36b53e75a7b2f20ebace84e96471bb
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-14 19:32:15 -0300

    definição de design e logina na tela de meus agendamentos

--- src/app/pages/meus-agendamentos/meus-agendamentos.page.ts
+++ src/app/pages/meus-agendamentos/meus-agendamentos.page.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Usuario } from 'src/app/interfaces/usuario';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-meus-agendamentos',
   templateUrl: './meus-agendamentos.page.html',
   styleUrls: ['./meus-agendamentos.page.scss'],
 })
-export class MeusAgendamentosPage implements OnInit {
+export class MeusAgendamentosPage {
+
+  public quantidadeAgendamentos: number;
 
   public usuario: Usuario = {};
+  public usuarioId: string = null;
+  private usuarioSubscription: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService,) { 
+    this.carregarDados()
   }
+
+  public async carregarDados(){
+    this.usuarioId = (await this.authService.getAuth().currentUser).uid;
+    this.usuarioSubscription = this.authService.getUsuario(this.usuarioId).subscribe(data => {
+      this.usuario = data;
+      this.quantidadeAgendamentos = this.usuario.agendamentos.length;
+    });
+  }
+
+
 
 }
