cd5c0cb2210d5aad1957b1eaaf7280cf9bd08ba1
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-09-14 23:49:56 -0300

    implementação da lógica na pagina de meus agendamentos

--- src/app/pages/meus-agendamentos/meus-agendamentos.page.ts
+++ src/app/pages/meus-agendamentos/meus-agendamentos.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Usuario } from 'src/app/interfaces/usuario';
 import { AuthService } from 'src/app/services/auth.service';
+import { DiaService } from 'src/app/services/dia.service';
 
 @Component({
   selector: 'app-meus-agendamentos',
@@ -16,18 +17,28 @@ export class MeusAgendamentosPage {
   public usuarioId: string = null;
   private usuarioSubscription: Subscription;
 
-  constructor(private authService: AuthService,) { 
+  constructor(private authService: AuthService,
+    private diaService: DiaService) { 
     this.carregarDados()
   }
 
   public async carregarDados(){
-    this.usuarioId = (await this.authService.getAuth().currentUser).uid;
+    this.usuarioId = (await this.authService.getAuth().currentUser).uid
     this.usuarioSubscription = this.authService.getUsuario(this.usuarioId).subscribe(data => {
       this.usuario = data;
       this.quantidadeAgendamentos = this.usuario.agendamentos.length;
     });
   }
 
+  public deletar(id: string){
+    console.log(this.usuario.agendamentos)
+    const index = this.usuario.agendamentos.findIndex(a => a.id === id)
+    this.usuario.agendamentos.splice(index, 1);
+    console.log(this.usuario.agendamentos)
+    this.authService.atualizarDados(this.usuarioId, this.usuario);
+    
+
+  }
 
 
 }
